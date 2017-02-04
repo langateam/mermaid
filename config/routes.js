d0bff17a6945d824a51c0fec51cf5d80b2194cfb
Travis J. Webb <me@traviswebb.com>
2017-02-04 01:21:43 -0500

    [config] add webhook route

--- config/routes.js
+++ config/routes.js
@@ -10,22 +10,9 @@
 'use strict'
 
 module.exports = [
-
-  /**
-   * Render the HelloWorld view
-   */
   {
-    method: 'GET',
-    path: '/',
-    handler: 'ViewController.helloWorld'
-  },
-
-  /**
-   * Constrain the DefaultController.info handler to accept only GET requests.
-   */
-  {
-    method: [ 'GET' ],
-    path: '/api/v1/default/info',
-    handler: 'DefaultController.info'
+    method: 'POST',
+    path: '/webhook',
+    handler: 'DockerhubController.webhook'
   }
 ]
